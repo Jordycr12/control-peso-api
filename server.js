18462c407a84bcfe17c71ea806431ae6db502734
Jordycr12 <alecarcamo12@gmail.com>
2025-08-05 19:32:27 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const registrosRoutes = require('./routes/registros');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors()); // <----- AQUI ACTIVAS CORS PARA TODOS LOS DOMINIOS
 app.use(express.json());
 app.use('/api', registrosRoutes);
 
